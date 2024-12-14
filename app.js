d7cb29a89735c5ff41e220aeb2dd274eab8f1faf
Widyputripratiwi <147219449+Widyputripratiwi@users.noreply.github.com>
2024-12-14 17:51:59 +0700

    Tugas 10
    Tugas 10 praktikum

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const express = require('express')
-const userRouter = require('./routes/user-route')
+const userRouter = require('./routes/user_route')
 
 dotenv.config()
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
 
 app.use('/api/users',userRouter)
 
-const port = process.env.port
+const port = process.env.PORT
 app.listen(port, ()=>{
     console.log(`Server run at http://localhost:${port}`);
 })
